fb7ee2e82ef300adc6ab140da381c9a23eec2638
Mateusz Macheta <mateusz.macheta@wp.pl>
2020-12-30 10:14:53 +0100

    praca nad szyfrem cezara

--- unit1.pas
+++ unit1.pas
@@ -1,3 +1,4 @@
+// Kryptografia - Mateusz Macheta 141147, 2020/21, wydzial techniki i informatyki, semestr V
 unit Unit1;
 
 {$mode objfpc}{$H+}
@@ -5,10 +6,17 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, LCLType;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    Button1: TButton;
+    Memo1: TMemo;
+    procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
 
   public
@@ -21,6 +29,78 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+Type ArrayOfByte =  Array of Byte;
+
+const
+CezarAlfabet: ArrayOfByte  = (65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
+81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 32, 44, 46, 140, 143, 163, 165, 175, 198,
+202, 209, 211);
+NazwaPliku = "OD_Labo_4_TEKST_ANSI.txt";
+
+function szyfrujCezar(Przesuniecie: Integer);
+var
+  plikTekstowy    : File of Char;
+  C : Char;
+  tekst: String;
+  i, indeks: integer;
+begin
+  AssignFile(plikTekstowy, NazwaPliku);
+  Reset(plikTekstowy);
+  tekst := '';
+
+   while not eof(plikTekstowy)
+     do begin
+       read(plikTekstowy, C);
+       tekst := tekst + C;
+     end;
+
+   CloseFile(plikTekstowy);
+
+   dlugoscPliku := Length(tekst);
+
+   Assignfile(plikTekstowy, 'ZakodowanyCezar.txt');
+   ReWrite(plikTekstowy);
+
+   for i := 1 to dlugoscPliku do
+    begin
+      C := CezarAlfabet[i];
+      Write(plikTekstowy, C);
+    end;
+
+   CloseFile(plikTekstowy);
+
+   Assignfile(plikTekstowy, 'szyfrogram.txt');
+   ReWrite(plikTekstowy);
+
+   for i := 1 to dlugoscPliku do
+    begin
+      C := Chr(Ord(tekst[i]) xor Ord(kluczVernama[i]));
+      Write(plikTekstowy, C);
+    end;
+
+   CloseFile(plikTekstowy);
+   Application.MessageBox('Plik oryginal.txt został zaszyfrowany kodem Vernama', 'Zaszyfrowanie');
+
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Application.MessageBox('Kryptografia - Mateusz Macheta 141147, 2020/21, wydzial techniki i informatyki, semestr V','Info',MB_OK);
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+var
+i: integer;
+begin
+     for i:=0 to 37 do
+     begin
+       Memo1.Append(chr(CezarAlfabet[i]));
+     end;
+end;
+
 
 end.
 
